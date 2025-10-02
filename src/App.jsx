62276f7b98190543159e5c77ee738e50bb8e123a
DBsan <39018564+dbsan2333@users.noreply.github.com>
2025-10-02 22:06:28 +0800

    🚧 完成TeamCard基本样式

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import "./App.css"
 import TeamCard from "./components/Ranking/TeamCard/TeamCard"
 
 function App() {
-	return <TeamCard />
+	return (
+		<>
+			<TeamCard />
+		</>
+	)
 }
 
 export default App
--- src/components/Ranking/TeamCard/TeamCard.jsx
+++ src/components/Ranking/TeamCard/TeamCard.jsx
@@ -1,9 +1,8 @@
 import "./TeamCard.scss"
-
-export function ProblemState({ state }) {
+function ProblemState({ state, id, data }) {
 	return (
 		<div className="problem-state" state={state}>
-			<span>4 - 234</span>
+			<span>{id}</span>
 		</div>
 	)
 }
@@ -12,20 +11,33 @@ export default function TeamCard() {
 	return (
 		<div className="team-card">
 			<div className="ranking">4</div>
-			<div className="org-logo"></div>
-			<div className="center">
-				<div className="org-name">郑州航空航天大学</div>
-				<div className="team-name"></div>
-				<div className="states">
-					<ProblemState />
-					<ProblemState state="AC-first" />
-					<ProblemState state="AC" />
-					<ProblemState state="WA" />
-					<ProblemState state="PD" />
-				</div>
-				<div className="solved-num">3</div>
-				<div className="total-time">1341</div>
+			<div className="org-logo">
+				<img src="/src/assets/img/logo_demo.png" />
 			</div>
+			<div className="center">
+				<div className="team-info">
+					<div className="org-name">郑州航空工业学院</div>
+					<div className="team-name">
+						咕咕嘎嘎咕咕嘎咕咕嘎咕咕嘎咕咕嘎咕咕嘎咕咕嘎咕咕嘎 咕咕嘎 咕咕嘎 咕咕嘎
+						咕咕嘎
+					</div>
+				</div>
+				<div className="states">
+					<ProblemState id="A" />
+					<ProblemState id="B" state="AC" />
+					<ProblemState id="C" state="AC" />
+					<ProblemState id="D" state="AC-first" />
+					<ProblemState id="E" />
+					<ProblemState id="F" state="WA" />
+					<ProblemState id="G" state="PD" />
+					<ProblemState id="H" />
+					<ProblemState id="I" />
+					<ProblemState id="J" />
+					<ProblemState id="K" />
+				</div>
+			</div>
+			<div className="solved-number">3</div>
+			<div className="total-time">1341</div>
 		</div>
 	)
 }
